20f5706e62e6f571498b9205bcad73ce30508f1c
fzxs <fzxs88@yeah.net>
2012-12-22 15:13:31 +0800

    t

---
+++ source/class/mongotool/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("mongotool.theme.Theme",
+{
+  meta :
+  {
+    color : mongotool.theme.Color,
+    decoration : mongotool.theme.Decoration,
+    font : mongotool.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : mongotool.theme.Appearance
+  }
+});
