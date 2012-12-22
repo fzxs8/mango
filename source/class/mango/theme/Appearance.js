84bc0d8a73f52e6fbe51e8afdc05d4359cbe9e19
fzxs <fzxs88@yeah.net>
2012-12-22 16:56:26 +0800

    修正

---
+++ source/class/mango/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("mango.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ source/class/mango/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("mango.theme.Theme",
+{
+  meta :
+  {
+    color : mango.theme.Color,
+    decoration : mango.theme.Decoration,
+    font : mango.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : mango.theme.Appearance
+  }
+});
