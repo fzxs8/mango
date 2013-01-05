49591baa7deb7770cfd2372e7f9a58fb4e24ff9c
fzxs <fzxs88@yeah.net>
2013-01-05 21:47:51 +0800

    备份

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.configure(function(){
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'source')));
 });
 
 app.configure('development', function(){
--- source/class/mango/Application.js
+++ source/class/mango/Application.js
@@ -6,70 +6,101 @@
 
    Authors:
 
-************************************************************************ */
+ ************************************************************************ */
 
 /* ************************************************************************
 
-#asset(mango/*)
+ #asset(mango/*)
 
-************************************************************************ */
+ ************************************************************************ */
 
 /**
  * This is the main application class of your custom application "Mango"
  */
-qx.Class.define("mango.Application",
-{
-  extend : qx.application.Standalone,
+qx.Class.define("mango.Application", {
+    extend : qx.application.Standalone,
 
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
-  members :
-  {
-    /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
-     * 
-     * @lint ignoreDeprecated(alert)
+    /*
+     * ****************************************************************************
+     * MEMBERS
+     * ****************************************************************************
      */
-    main : function()
-    {
-      // Call super class
-      this.base(arguments);
 
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
+    members : {
+        /**
+         * This method contains the initial application code and gets called
+         * during startup of the application
+         * 
+         * @lint ignoreDeprecated(alert)
+         */
+        main : function() {
+            // Call super class
+            this.base(arguments);
 
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
+            // Enable logging in debug variant
+            if (qx.core.Environment.get("qx.debug")) {
+                // support native logging capabilities, e.g. Firebug for Firefox
+                qx.log.appender.Native;
+                // support additional cross-browser console. Press F7 to toggle
+                // visibility
+                qx.log.appender.Console;
+            }
 
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "mango/test.png");
+            /*
+             * -------------------------------------------------------------------------
+             * Below is your actual application code...
+             * -------------------------------------------------------------------------
+             */
+            var scroll = new qx.ui.container.Scroll();
+            this.getRoot().add(scroll, {
+                edge : 0
+            });
 
-      // Document is the application root
-      var doc = this.getRoot();
+            var container = new qx.ui.container.Composite(new qx.ui.layout.Flow()).set({
+              //  padding : 200,
+                width : 1000
+            })
+            // container.setHeight(1400);
+            // container.setWidth(1400);
+            scroll.add(container);
+            // default layout, auto-sized
+            var widget = new qx.ui.container.Composite(new qx.ui.layout.Dock()).set({
+                decorator : "main",
+                backgroundColor : "gray",
+                allowGrowX : false
+            });
 
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
+            var w1 = new qx.ui.core.Widget().set({
+                decorator : "main",
+                backgroundColor : "red"
+            });
 
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
-      });
+            var splitpane = new qx.ui.splitpane.Pane("horizontal");
+            // splitpane.setWidth(400);
+            // splitpane.setHeight(60);
+            splitpane.setDecorator("main");
+
+            // Left
+            var leftWidget = new qx.ui.form.TextArea("Flex:1");
+            leftWidget.setDecorator(null);
+            leftWidget.setWrap(true);
+            splitpane.add(leftWidget, 1);
+
+            // Right
+            var rightWidget = new qx.ui.form.TextArea("Flex:2");
+            rightWidget.setDecorator(null);
+            rightWidget.setWrap(true);
+            splitpane.add(rightWidget, 2);
+
+            widget.add(w1, {
+                edge : "north"
+            });
+            widget.add(splitpane, {
+                edge : "west"
+            });
+
+            container.add(widget);
+        }
     }
-  }
+
 });
